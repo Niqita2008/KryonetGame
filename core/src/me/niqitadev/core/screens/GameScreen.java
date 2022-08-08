ea24bfaf7c1330bfda91c81eda13f812c9e67955
niqitadev <bondarenkonikita951480@gmail.com>
2022-08-08 09:40:43 +0500

    join request and response

--- core/src/me/niqitadev/core/screens/GameScreen.java
+++ core/src/me/niqitadev/core/screens/GameScreen.java
@@ -3,18 +3,29 @@ package me.niqitadev.core.screens;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
 
 public class GameScreen extends ScreenAdapter {
-    public GameScreen(OrthographicCamera camera, SpriteBatch spriteBatch) {
+    private final Stage stage;
+    private final OrthographicCamera camera;
+    private final StretchViewport viewport;
 
+    public GameScreen(OrthographicCamera camera, SpriteBatch spriteBatch) {
+        this.camera = camera;
+        viewport = new StretchViewport(4, 3, camera);
+        stage = new Stage(viewport, spriteBatch);
     }
 
     @Override
     public void render(float delta) {
+        stage.draw();
     }
 
     @Override
     public void resize(int width, int height) {
+        viewport.update(width, height);
+        camera.update();
     }
 
     @Override
